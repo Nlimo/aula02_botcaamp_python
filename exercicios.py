d1c135a612245005a30f003532a04fc08087e986
Nlimo <lucaswilliamschaves@gmail.com>
2024-10-08 18:54:05 -0300

    add codigo da questao 14

--- exercicios.py
+++ exercicios.py
@@ -90,11 +90,20 @@
 
 # 13. Desenvolva um programa que peça ao usuário para inserir uma frase e, 
 # em seguida, imprima esta frase sem espaços em branco no início e no final.
-var = input("Digite uma frase: ")
-result13 = var.strip()
-print(result13)
+#var = input("Digite uma frase: ")
+#result13 = var.strip()
+#print(result13)
 
-# 14. Faça um programa que peça ao usuário para digitar uma data no formato "dd/mm/aaaa" e, em seguida, imprima o dia, o mês e o ano separadamente.
+# 14. Faça um programa que peça ao usuário para digitar uma data no formato "dd/mm/aaaa" e, 
+# em seguida, imprima o dia, o mês e o ano separadamente.
+var = input("Digite uma data 'dd/mm/aaaa': ")
+
+result14 = var.split("/")
+
+print(result14[0])
+print(result14[1])
+print(result14[2])
+
 # 15. Escreva um programa que concatene duas strings fornecidas pelo usuário.
 
 # #### Booleanos (`bool`)
