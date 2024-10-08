616203d943372276ce5c8564fb1bf0216e026ab0
Nlimo <lucaswilliamschaves@gmail.com>
2024-10-07 23:29:24 -0300

    add codigo do segundo exercicio

--- exercicios.py
+++ exercicios.py
@@ -1,13 +1,16 @@
 # #### Inteiros (`int`)
 
 # 1. Escreva um programa que soma dois números inteiros inseridos pelo usuário.
-num1 = float(input("Insira o primeiro número: "))
-num2 = float(input("Insira o segundo número: "))
+#num1 = float(input("Insira o primeiro número: "))
+#num2 = float(input("Insira o segundo número: "))
 
-result1 = num1 + num2
+#result1 = num1 + num2
 
-print(f"O resultado é {result1}")
+#print(f"O resultado é {result1}")
 # 2. Crie um programa que receba um número do usuário e calcule o resto da divisão desse número por 5.
+num = float(input("Insira um número: "))
+result2 = num % 3
+print(f"O resto da divisão de {num} por 5 é: {result2}")
 # 3. Desenvolva um programa que multiplique dois números fornecidos pelo usuário e mostre o resultado.
 # 4. Faça um programa que peça dois números inteiros e imprima a divisão inteira do primeiro pelo segundo.
 # 5. Escreva um programa que calcule o quadrado de um número fornecido pelo usuário.
