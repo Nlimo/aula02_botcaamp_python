42877095868a39ac34aef61ccc37ec44fce6f31d
Nlimo <lucaswilliamschaves@gmail.com>
2024-10-08 18:48:22 -0300

    add coidgo da questao 13 e ajuste enos nomes das variaveis na 11 e 12

--- exercicios.py
+++ exercicios.py
@@ -80,15 +80,20 @@
 
 # 11. Escreva um programa que receba uma string do usuário e a converta para maiúsculas.
 #var = input("Digite uma palavra: ")
-#result = var.upper()
-#print(result)
+#result11 = var.upper()
+#print(result11)
 
 # 12. Crie um programa que receba o nome completo do usuário e imprima o nome com todas as letras minúsculas.
 #var = input("Digite seu nome completo: ")
-#result = var.lower()
-#print(result)
+#result12 = var.lower()
+#print(result12)
 
-# 13. Desenvolva um programa que peça ao usuário para inserir uma frase e, em seguida, imprima esta frase sem espaços em branco no início e no final.
+# 13. Desenvolva um programa que peça ao usuário para inserir uma frase e, 
+# em seguida, imprima esta frase sem espaços em branco no início e no final.
+var = input("Digite uma frase: ")
+result13 = var.strip()
+print(result13)
+
 # 14. Faça um programa que peça ao usuário para digitar uma data no formato "dd/mm/aaaa" e, em seguida, imprima o dia, o mês e o ano separadamente.
 # 15. Escreva um programa que concatene duas strings fornecidas pelo usuário.
 
